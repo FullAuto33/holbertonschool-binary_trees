2e11ebc457f8a9002b56a05548486d3771286128
root <djelloulrouba@yahoo.com>
2024-12-12 09:04:15 +0000

    Add all comment fofr Basic Binary Tree

--- binary_trees.h
+++ binary_trees.h
@@ -20,7 +20,7 @@ struct binary_tree_s *left;
 struct binary_tree_s *right;
 };
 
-typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s binary_tree_t; /* Basic Binary Tree */
 typedef struct binary_tree_s bst_t; /* Binary Search Tree */
 typedef struct binary_tree_s avl_t; /* AVL Tree */
 typedef struct binary_tree_s heap_t;/* Max Binary Heap */
