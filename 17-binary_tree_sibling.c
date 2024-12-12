9c7338760fc278d2b9f74c3035abe1e6169fb940
root <djelloulrouba@yahoo.com>
2024-12-12 14:46:01 +0000

    Script 17 function sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,16 +7,10 @@ if (node == NULL || node->parent)
 return (NULL);
 }
 if (node->parent->left == node)
-{
-node = node->parent->right;
+{		return (node->parent->right);
 }
-else if (node->parent->left == NULL || node->parent->right == NULL)
+else
 {
-return (NULL);
+return (node->parent->left);
 }
-else if (node->parent->right == node)
-{
-node = node->parent->left;
-}
-return (node);
 }
