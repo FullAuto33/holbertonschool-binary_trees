58bd26ff1eab20e04be632c6be8942ba4f66af5f
root <djelloulrouba@yahoo.com>
2024-12-12 13:33:22 +0000

    Add bety style and add one in return for the node root

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,11 +1,16 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - size of treee
+ * @tree: pointer to tree
+ * Return: Always 0 (Success)
+ */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    return (binary_tree_size(tree->left) + binary_tree_size(tree->right));
-
+if (tree == NULL)
+{
+return (0);
+}
+return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
