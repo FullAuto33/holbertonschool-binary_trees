ee3e9d4481f8c4531f11bfa495a25f56aef180ec
root <djelloulrouba@yahoo.com>
2024-12-12 09:03:38 +0000

    Add all prototype in header file and comment the header

--- binary_trees.h
+++ binary_trees.h
@@ -21,14 +21,30 @@ struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s bst_t; /* Binary Search Tree */
+typedef struct binary_tree_s avl_t; /* AVL Tree */
+typedef struct binary_tree_s heap_t;/* Max Binary Heap */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *);
-
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *); /* Function Print */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value); /* Funtion 0 */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);/* Function 1 */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value); /* Function 2 */
+void binary_tree_delete(binary_tree_t *tree); /* Function 3 */
+int binary_tree_is_leaf(const binary_tree_t *node); /* Function 4 */
+int binary_tree_is_root(const binary_tree_t *node); /* Function 5 */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)); /* Function 6 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)); /* Function 7 */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)); /* Function 8 */
+size_t binary_tree_height(const binary_tree_t *tree); /* Function 9 */
+size_t binary_tree_depth(const binary_tree_t *tree); /* Function 10 */
+size_t binary_tree_size(const binary_tree_t *tree); /* Function 11 */
+size_t binary_tree_leaves(const binary_tree_t *tree); /* Function 12 */
+size_t binary_tree_nodes(const binary_tree_t *tree); /* Function 13 */
+int binary_tree_balance(const binary_tree_t *tree); /* Function 14 */
+int binary_tree_is_full(const binary_tree_t *tree); /* Function 15 */
+int binary_tree_is_perfect(const binary_tree_t *tree); /* Function 16 */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node); /* Function 17 */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node); /* Function 18 */
 
 
 #endif
