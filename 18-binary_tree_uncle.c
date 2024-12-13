152a3065f9bde2469c5c299b04f8949c8a0039a3
root <djelloulrouba@yahoo.com>
2024-12-13 09:34:23 +0000

    Fix problem if dont have parent with replace grand parent to paren

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,11 +8,11 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (node == NULL || node->parent->parent == NULL)
+if (node == NULL || node->parent == NULL)
 {
 return (NULL);
 }
-if (node->parent == NULL)
+if (node->parent->parent == NULL)
 {
 return (NULL);
 }
