5a086078005008a3a27a0750be71419b319320a1
root <djelloulrouba@yahoo.com>
2024-12-13 10:53:10 +0000

    add documentation fo function for betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_full - verify if tree is full or no
+ * @tree: pointer to tree
+ * Return: Always 0 (Success)
+ */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
