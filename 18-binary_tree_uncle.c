c8fff884082bf8ca69e2ae94b14c448bee9ae1f7
root <djelloulrouba@yahoo.com>
2024-12-13 09:22:21 +0000

    add betty style and documentation for function and fix problem if dont have node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,13 +1,23 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_uncle - find uncle to node
+ * @node: pointer of node
+ * Return: Always 0 (Success)
+ */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node->parent->parent->left == node->parent)
-    {
-        return (node->parent->parent->right);
-    }
-    else
-    {
-	return (node->parent->parent->left);
-    }
+if (node == NULL || node->parent == NULL)  
+{
+return (NULL);
+}
+if (node->parent->parent->left == node->parent)
+{
+return (node->parent->parent->right);
+}
+else
+{
+return (node->parent->parent->left);
+}
 }
